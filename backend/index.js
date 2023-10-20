90b071a078f08723ee67609b024d65c31c744105
Rajnikant1021 <rajnikant1021@gmail.com>
2023-10-21 03:50:35 +0530

    gh

--- backend/index.js
+++ backend/index.js
@@ -4,10 +4,13 @@ const cors=require('cors');
 
 connectTomongo();
 const app=express();
-const port=5000;
+const port=process.env.PORT || 5000;
 app.use(cors());
 app.use(express.json());//for dealing data in json
 //available routes
+if(process.env.NODE_ENV == "production"){
+    app.use(express.static("frontend/build"));
+}
 app.use('/api/auth',require('./routes/auth'))
 app.use('/api/notes',require('./routes/notes'))
 app.get('/',(req,res)=>{
