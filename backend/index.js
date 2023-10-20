53af15bd3900eb33b34bdb6352718bca85af38fa
Rajnikant1021 <rajnikant1021@gmail.com>
2023-10-21 05:27:07 +0530

    sadsf

--- backend/index.js
+++ backend/index.js
@@ -1,30 +1,32 @@
-const connectTomongo=require('./db');
-const express=require('express');
-const cors=require('cors');
+const connectToMongo = require('./db');
+const express = require('express');
+const cors = require('cors');
 
-connectTomongo();
-const app=express();
-const port=5000;
+connectToMongo();
+
+const app = express();
+const port = 5000;
+
+// Enable CORS with specific options
 app.use(
-    cors({
-        origin: "https://inotebook-ur-cloud-notes.vercel.app",
-      preflightContinue: true,
-      methods: "GET,HEAD,PUT,PATCH,POST,DELETE",
-      credentials: true,
-    })
-  );
+  cors({
+    origin: "https://inotebook-ur-cloud-notes.vercel.app", // Set your frontend URL here
+    methods: "GET,HEAD,PUT,PATCH,POST,DELETE",
+    credentials: true,
+    preflightContinue: false, // By default, preflight requests are handled, so no need to set this explicitly
+  })
+);
 
-app.use(express.json());//for dealing data in json
-//available routes
-// if(process.env.NODE_ENV == "production"){
-//     app.use(express.static("frontend/build"));
-// }
-app.use('/api/auth',require('./routes/auth'))
-app.use('/api/notes',require('./routes/notes'))
-app.get('/',(req,res)=>{
-    res.send('hello world')
+app.use(express.json()); // Middleware for parsing JSON request bodies
 
-})
-app.listen(port,()=>{
-    console.log(`app listeninig on port ${port}`)
-})
+// Define your routes
+app.use('/api/auth', require('./routes/auth'));
+app.use('/api/notes', require('./routes/notes'));
+
+app.get('/', (req, res) => {
+  res.send('hello world');
+});
+
+app.listen(port, () => {
+  console.log(`App listening on port ${port}`);
+});
