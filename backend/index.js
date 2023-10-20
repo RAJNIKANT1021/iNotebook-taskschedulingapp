d7d35849a3e69a6526cbe2185b947c3bb3b8be7b
Rajnikant1021 <rajnikant1021@gmail.com>
2023-10-21 04:42:56 +0530

    okk

--- backend/index.js
+++ backend/index.js
@@ -4,7 +4,7 @@ const cors=require('cors');
 
 connectTomongo();
 const app=express();
-const port=process.env.PORT || 5000;
+const port=5000;
 app.use(cors (
     {
     origin: ["https://deploy-mern-1whq.vercel.app"],
@@ -15,9 +15,9 @@ app.use(cors (
     ));
 app.use(express.json());//for dealing data in json
 //available routes
-if(process.env.NODE_ENV == "production"){
-    app.use(express.static("frontend/build"));
-}
+// if(process.env.NODE_ENV == "production"){
+//     app.use(express.static("frontend/build"));
+// }
 app.use('/api/auth',require('./routes/auth'))
 app.use('/api/notes',require('./routes/notes'))
 app.get('/',(req,res)=>{
